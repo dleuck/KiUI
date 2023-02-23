dd99e5f90d37b74581745f14e88d91155c47ff3e
Daniel Leuck <dan@ikayzo.com>
2023-02-23 18:51:45 +0900

    Added string rep for palette

--- Sources/KiUI/Palette.swift
+++ Sources/KiUI/Palette.swift
@@ -9,7 +9,13 @@ import Foundation
 
 
 
-public struct Palette: Codable {
+public struct Palette: Codable, CustomStringConvertible {
+    
+    public let name: String
+    
+    public let primary: RGB
+    public let secondary: RGB
+    public let tertiary: RGB
     
     public static let defaultPalette = Palette(
         name: "default", RGB(102, 71, 216), RGB(43, 139, 49), RGB(208, 178, 26)
@@ -20,18 +26,12 @@ public struct Palette: Codable {
     )
     
     public static let rainbowPalette = Palette(
-        name: "rainbow", RGB(50, 127, 97), RGB(229, 161, 10), RGB(216, 58, 50)
+        name: "rainbow", RGB(163, 130, 212), RGB(137, 195, 233), RGB(250, 234, 144)
     )
     
     public static let socialPalette = Palette(
         name: "social", RGB(12, 198, 171), RGB(84, 33, 244), RGB(206, 158, 5)
     )
-    
-    public let name: String
-    
-    public let primary: RGB
-    public let secondary: RGB
-    public let tertiary: RGB
     
     public init(name: String, _ primary: RGB, _ secondary: RGB, _ tertiary: RGB) {
         self.name = name
@@ -39,5 +39,9 @@ public struct Palette: Codable {
         self.primary = primary
         self.secondary = secondary
         self.tertiary = tertiary
+    }
+    
+    public var description: String {
+        "\(name) - primary:\(primary), secondary:\(secondary), tertiary:\(tertiary)"
     }
 }
