0785137f611fd705d3e20776cee679dd42f73975
dleuck <dan@ikayzo.com>
2023-03-09 17:27:18 +0900

    Improved API and fixed description property for Palette struct.

--- Sources/KiUI/Palette.swift
+++ Sources/KiUI/Palette.swift
@@ -17,34 +17,33 @@ public struct Palette: Codable, CustomStringConvertible {
     // TODO: Add standard named variants - pastel, veryLight, light, base, dark, varyDark
     
     public let name: String
-    
     public let primary: RGB
     public let secondary: RGB
     public let tertiary: RGB
     public let quaternary: RGB
     public let danger: RGB
-    
-    public static let defaultPalette = Palette(
+
+    public static let `default` = Palette(
         name: "default", webRGB(102, 71, 216), webRGB(43, 139, 49), webRGB(208, 178, 26)
     )
     
-    public static let naturePalette = Palette(
+    public static let nature = Palette(
         name: "default", webRGB(67, 137, 194), webRGB(149, 200, 46), webRGB(218, 175, 63)
     )
     
-    public static let trafficLightPalette = Palette(
+    public static let trafficLight = Palette(
         name: "traffic", webRGB(50, 127, 97), webRGB(229, 161, 10), webRGB(216, 58, 50)
     )
     
-    public static let rainbowPalette = Palette(
+    public static let rainbow = Palette(
         name: "rainbow", webRGB(163, 130, 212), webRGB(137, 195, 233), webRGB(250, 234, 144)
     )
     
-    public static let socialPalette = Palette(
+    public static let social = Palette(
         name: "social", webRGB(12, 198, 171), webRGB(84, 33, 244), webRGB(206, 158, 5)
     )
     
-    public static let nouveauPalette = Palette(
+    public static let nouveau = Palette(
         name: "social", webRGB(57, 174, 160), webRGB(111, 62, 193), webRGB(217, 108, 20),
             webRGB(125, 168, 53)
     )
@@ -78,6 +77,9 @@ public struct Palette: Codable, CustomStringConvertible {
     }
     
     public var description: String {
-        "\(name) - primary:\(primary), secondary:\(secondary), tertiary:\(tertiary)"
+        return getScheme() == .triadic
+            ? "\(name) - primary:\(primary), secondary:\(secondary), tertiary:\(tertiary)"
+            : "\(name) - primary:\(primary), secondary:\(secondary), tertiary:\(tertiary) " +
+              "quaternary:\(quaternary)"
     }
 }
